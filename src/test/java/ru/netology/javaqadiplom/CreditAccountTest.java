8128d55c7fb22f1d058162241304eb7ec44e2228
Kate Bo <bodrakea@gmail.com>
2024-01-26 17:12:28 +0300

    add tests

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -4,17 +4,129 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CreditAccountTest {
+    final int initialBalance = 1000;
+    final int creditLimit = 5_000;
+    final int rate = 15;
+    CreditAccount account = new CreditAccount(initialBalance, creditLimit, rate);
+
+    @Test
+    public void shouldCreateAccount() {
+        Assertions.assertEquals(initialBalance, account.getBalance());
+        Assertions.assertEquals(creditLimit, account.getCreditLimit());
+        Assertions.assertEquals(rate, account.getRate());
+    }
+
+    @Test
+    public void shouldGenerateExceptionByRate() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CreditAccount(initialBalance, creditLimit, -rate));
+    }
+
+    @Test
+    public void shouldGenerateExceptionByLimit() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CreditAccount(initialBalance, -creditLimit, rate));
+    }
+
+    @Test
+    public void shouldGenerateExceptionByInitBalance() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CreditAccount(-initialBalance, creditLimit, rate));
+    }
+
+    @Test
+    public void shouldPayUnderLimit() {
+        int amount = creditLimit + initialBalance - 1;
+
+        boolean expectedPay = true;
+        boolean actualPay = account.pay(amount);
+        int expectedBalance = initialBalance - amount;
+        int actualBalance = account.getBalance();
+
+        Assertions.assertEquals(expectedPay, actualPay);
+        Assertions.assertEquals(expectedBalance, actualBalance);
+    }
+
+    @Test
+    public void shouldPayEqualLimit() {
+        int amount = creditLimit + initialBalance;
+
+        boolean expectedPay = true;
+        boolean actualPay = account.pay(amount);
+        int expectedBalance = initialBalance - amount;
+        int actualBalance = account.getBalance();
+
+        Assertions.assertEquals(expectedPay, actualPay);
+        Assertions.assertEquals(expectedBalance, actualBalance);
+    }
+
+    @Test
+    public void shouldNotPayOverLimit() {
+        int amount = creditLimit + initialBalance + 1;
+
+        boolean expectedPay = false;
+        boolean actualPay = account.pay(amount);
+        int expectedBalance = initialBalance;
+        int actualBalance = account.getBalance();
+
+        Assertions.assertEquals(expectedPay, actualPay);
+        Assertions.assertEquals(expectedBalance, actualBalance);
+
+    }
+
+    @Test
+    public void shouldNotPayAmountNegative() {
+        int amount = -1;
+
+        boolean expectedPay = false;
+        boolean actualPay = account.pay(amount);
+        int expectedBalance = initialBalance;
+        int actualBalance = account.getBalance();
+
+        Assertions.assertEquals(expectedPay, actualPay);
+        Assertions.assertEquals(expectedBalance, actualBalance);
+
+    }
 
     @Test
     public void shouldAddToPositiveBalance() {
-        CreditAccount account = new CreditAccount(
-                0,
-                5_000,
-                15
-        );
+        int amount = 1;
 
-        account.add(3_000);
+        boolean expectedAdd = true;
+        boolean actualAdd = account.add(amount);
+        int expectedBalance = initialBalance + amount;
+        int actualBalance = account.getBalance();
 
-        Assertions.assertEquals(3_000, account.getBalance());
+        Assertions.assertEquals(expectedBalance, actualBalance);
+        Assertions.assertEquals(expectedAdd, actualAdd);
     }
+
+    @Test
+    public void shouldNotAddNegativeBalance() {
+        int amount = -1;
+
+        boolean expectedAdd = false;
+        boolean actualAdd = account.add(amount);
+        int expectedBalance = initialBalance;
+        int actualBalance = account.getBalance();
+
+        Assertions.assertEquals(expectedBalance, actualBalance);
+        Assertions.assertEquals(expectedAdd, actualAdd);
+    }
+
+    @Test
+    public void shouldCalculateChange() {
+        account.pay(creditLimit);
+
+        int expected = (initialBalance - creditLimit) / 100 * rate;
+        int actual = account.yearChange();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotCalculateChange(){
+        int expected = 0;
+        int actual = account.yearChange();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
